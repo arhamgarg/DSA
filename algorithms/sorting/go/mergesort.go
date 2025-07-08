c4792732c55ff597f3b2473d8299d110fb36c49e
Dharshan2208 <dharshan080622@gmail.com>
2025-07-08 19:33:21 +0530

    added : golang data structures and algorithms

---
+++ algorithms/sorting/go/mergesort.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	// divide
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	// conuqer
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	i, j := 0, 0
+
+	// Merge two sorted slices
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+
+	// Append remaining elements
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
+}
+
+func main() {
+	arr := []int{4, 1, 47, 64, 23, 6, 37,69}
+
+	sorted := mergeSort(arr)
+	fmt.Println("Sorted array :  ", sorted)
+}
