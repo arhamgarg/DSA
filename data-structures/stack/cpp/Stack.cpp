75ceb00fa5cbd81820ea10d61bf0f8b8595697fe
arhamgarg <arhamvgarg@gmail.com>
2025-07-10 19:09:24 +0530

    feat: implement stack using singly linked list

---
+++ data-structures/stack/cpp/Stack.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+  int data;
+  Node *next;
+
+  Node(int value) {
+    data = value;
+    next = nullptr;
+  }
+};
+
+class Stack {
+  Node *head;
+  int size;
+
+public:
+  Stack() {
+    head = nullptr;
+    size = 0;
+  }
+
+  void push(int value) {
+    Node *newNode = new Node(value);
+
+    if (head == nullptr) {
+      head = newNode;
+      size++;
+      return;
+    }
+
+    newNode->next = head;
+    head = newNode;
+    size++;
+  }
+
+  void pop() {
+    if (head == nullptr) {
+      cout << "Stack is empty!\n";
+      return;
+    }
+
+    Node *temp = head;
+    head = head->next;
+    temp->next = nullptr;
+    delete temp;
+    size--;
+  }
+
+  int top() { return head->data; }
+
+  bool isEmpty() { return size == 0; }
+};
