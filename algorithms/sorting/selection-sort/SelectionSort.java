a620ea39cd1d5cfd77005f30a536961c8280c5b1
Dharshan2208 <dharshan080622@gmail.com>
2025-08-03 14:06:38 +0530

    add : searching,sorting and binary tree in both go and java

---
+++ algorithms/sorting/selection-sort/SelectionSort.java
@@ -0,0 +1,16 @@
+public class SelectionSort {
+    public static int[] selectionsort(int[] array) {
+        for (int i = 0; i <= array.length - 1; i++) {
+            int min = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            int temp = array[min];
+            array[min] = array[i];
+            array[i] = temp;
+        }
+        return array;
+    }
+}
