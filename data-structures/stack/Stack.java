bf9c43d8d7cb19975e6396c8ffad8666c7a77161
Arham Garg <arhamvgarg@gmail.com>
2025-08-07 20:48:25 +0530

    Update Stack.java

--- data-structures/stack/Stack.java
+++ data-structures/stack/Stack.java
@@ -1,52 +1,51 @@
+public class Node {
+  int data;
+  Node next;
+
+  public Node(int data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
 public class Stack {
-  Node head;
+  private Node head;
   private int size;
 
-  class Node {
-    int data;
-    Node next;
-
-    Node(int data) {
-      this.data = data;
-      this.next = null;
-    }
+  public Stack() {
+    head = null;
+    size = 0;
   }
 
   public void push(int data) {
     Node newNode = new Node(data);
-
-    if (head == null) {
-      head = newNode;
-      size++;
-      return;
-    }
-
     newNode.next = head;
     head = newNode;
     size++;
   }
 
-  public int pop() {
+  public void pop() {
     if (head == null) {
       System.out.println("Stack is empty");
     }
 
-    int value = head.data;
     head = head.next;
     size--;
-
-    return value;
   }
 
   public int top() {
-    return head.data;
-  }
+    if (head == null) {
+      System.out.println("Stack is empty");
+    }
 
-  public int size() {
-    return size;
+    return head.data;
   }
 
   public boolean isEmpty() {
     return size == 0;
+  }
+
+  public int size() {
+    return size;
   }
 }
