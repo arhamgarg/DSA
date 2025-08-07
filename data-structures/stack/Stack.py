fa16fb24100a0b970c5ba59a918d55a1f60bee3e
Arham Garg <arhamvgarg@gmail.com>
2025-08-07 20:46:07 +0530

    Update Stack.py

--- data-structures/stack/Stack.py
+++ data-structures/stack/Stack.py
@@ -4,7 +4,7 @@ class Node:
         self.next = None
 
 
-class StackLL:
+class Stack:
     def __init__(self):
         self.head = None
         self._size = 0
@@ -14,32 +14,20 @@ class StackLL:
         new_node.next = self.head
         self.head = new_node
         self._size += 1
-        self.display()
 
     def pop(self):
         if self.is_empty():
             print("Stack Underflow")
-        popped = self.head.data
         self.head = self.head.next
         self._size -= 1
-        print(popped)
-        self.display()
 
     def top(self):
         if self.is_empty():
             print("Stack Underflow")
-        else:
-            print(self.head.data)
+        print(self.head.data)
 
     def is_empty(self):
         return self.head is None
 
     def size(self):
         return self._size
-
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" -> ")
-            current = current.next
-        print("None")
