46eaa552e16480ca03829cd4af06a13ff3dafb36
Arham Garg <arhamvgarg@gmail.com>
2025-08-07 21:45:17 +0530

    Update Queue.py

--- data-structures/queue/Queue.py
+++ data-structures/queue/Queue.py
@@ -4,7 +4,7 @@ class Node:
         self.next = None
 
 
-class QueueLL:
+class Queue:
     def __init__(self):
         self.head = None
         self._size = 0
@@ -19,33 +19,20 @@ class QueueLL:
                 curr = curr.next
             curr.next = new_node
         self._size += 1
-        self.display()
 
     def dequeue(self):
         if self.is_empty():
             print("Queue is empty")
-        popped = self.head.data
         self.head = self.head.next
         self._size -= 1
-        print(popped)
-        self.display()
 
     def top(self):
         if self.is_empty():
             print("Queue is empty")
-        else:
-            print(self.head.data)
-            self.display()
+        print(self.head.data)
 
     def is_empty(self):
         return self.head is None
 
     def size(self):
         return self._size
-
-    def display(self):
-        current = self.head
-        while current:
-            print(current.data, end=" -> ")
-            current = current.next
-        print("None")
