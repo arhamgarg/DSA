936be5a4d186a7a43bcd0158ace7c14822a6fbfc
Ritesh Koushik <riteshkoushik39@gmail.com>
2025-08-12 12:28:05 +0530

    fix: Formatting adhers to gofmt, error handling in top(), pop()

--- data-structures/stack/Stack.go
+++ data-structures/stack/Stack.go
@@ -1,51 +1,49 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type StackNode struct {
-    data int
-    next * StackNode
+	data int
+	next *StackNode
 }
 
 type Stack struct {
-    head * StackNode
-    size int
+	head *StackNode
+	size int
 }
 
-func(s * Stack) Push(data int) {
-    newNode:= & StackNode {
-        data: data,
-    }
-    newNode.next = s.head
-    s.head = newNode
-    s.size++
+func (s *Stack) Push(data int) {
+	newNode := &StackNode{
+		data: data,
+	}
+	newNode.next = s.head
+	s.head = newNode
+	s.size++
 }
 
-func(s * Stack) Pop() int {
-    if s.head == nil {
-        fmt.Println("Stack is empty")
-        return -1
-    }
-    val:= s.head.data
-    s.head = s.head.next
-    s.size--
-        return val
+func (s *Stack) Pop() (int, error) {
+	if s.head == nil {
+		return 0, fmt.Errorf("stack is empty")
+	}
+	val := s.head.data
+	s.head = s.head.next
+	s.size--
+	return val, nil
 }
 
-func(s * Stack) Top() int {
-    if s.head == nil {
-        fmt.Println("Stack is empty")
-        return -1
-    }
-    return s.head.data
+func (s *Stack) Top() (int, error) {
+	if s.head == nil {
+		return 0, fmt.Errorf("stack is empty")
+	}
+	return s.head.data, nil
 }
 
-func(s * Stack) Size() int {
-    return s.size
+func (s *Stack) Size() int {
+	return s.size
 }
 
-func(s * Stack) IsEmpty() bool {
-    return s.size == 0
+func (s *Stack) IsEmpty() bool {
+	return s.size == 0
 }
