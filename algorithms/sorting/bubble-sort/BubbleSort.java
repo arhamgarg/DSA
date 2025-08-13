6fcf5a16ad89fe1489fb3bc49449105dafa1c850
Arham Garg <arhamvgarg@gmail.com>
2025-08-14 02:50:33 +0530

    style: format BubbleSort.java for consistent indentation and readability

--- algorithms/sorting/bubble-sort/BubbleSort.java
+++ algorithms/sorting/bubble-sort/BubbleSort.java
@@ -1,21 +1,22 @@
 public class BubbleSort {
-    public static void bubbleSort(int[] nums) {
-        if (nums == null || nums.length < 2) return;
+  public static void bubbleSort(int[] nums) {
+    if (nums == null || nums.length < 2)
+      return;
 
-        boolean swapped = true;
-        int end = nums.length;
+    boolean swapped = true;
+    int end = nums.length;
 
-        while (swapped) {
-            swapped = false;
-            for (int i = 0; i < end - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    int temp = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = temp;
-                    swapped = true;
-                }
-            }
-            end--;
+    while (swapped) {
+      swapped = false;
+      for (int i = 0; i < end - 1; i++) {
+        if (nums[i] > nums[i + 1]) {
+          int temp = nums[i];
+          nums[i] = nums[i + 1];
+          nums[i + 1] = temp;
+          swapped = true;
         }
+      }
+      end--;
     }
+  }
 }
