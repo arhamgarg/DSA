edc5e5b1faca82a8457bf0c154d19ed7ec85148d
Arham Garg <arhamvgarg@gmail.com>
2025-08-15 15:29:29 +0530

    ref: remove redundant comments and format

--- data-structures/node/Node.cpp
+++ data-structures/node/Node.cpp
@@ -1,27 +1,17 @@
 class Node {
 private:
-    int value;     // Node's value
-    Node* nextNode; // Pointer to the next node
+  int value;
+  Node *nextNode;
 
 public:
-    // Constructor
-    Node(int val, Node* next = nullptr) {
-        value = val;
-        nextNode = next;
-    }
+  Node(int val, Node *next = nullptr) {
+    value = val;
+    nextNode = next;
+  }
 
-    // Getter for value
-    int getValue() const {
-        return value;
-    }
+  int getValue() const { return value; }
 
-    // Getter for next node
-    Node* getNextNode() const {
-        return nextNode;
-    }
+  Node *getNextNode() const { return nextNode; }
 
-    // Setter for next node
-    void setNextNode(Node* next) {
-        nextNode = next;
-    }
+  void setNextNode(Node *next) { nextNode = next; }
 };
