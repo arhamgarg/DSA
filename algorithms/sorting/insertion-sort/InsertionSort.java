4a7e3c87d020b8560c67af1e77a361298ebc872a
Arham Garg <arhamvgarg@gmail.com>
2025-08-16 14:28:23 +0530

    style: format InsertionSort.java for consistent indentation and readability

--- algorithms/sorting/insertion-sort/InsertionSort.java
+++ algorithms/sorting/insertion-sort/InsertionSort.java
@@ -1,24 +1,23 @@
 public class InsertionSort {
+  public static int[] insertionSort(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      boolean swapped = false;
+      int current = arr[i];
+      int j = i - 1;
 
-    public static int[] insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            boolean swapped = false;
-            int current = arr[i];
-            int j = i - 1;
+      while (j >= 0 && arr[j] > current) {
+        arr[j + 1] = arr[j];
+        j--;
+        swapped = true;
+      }
 
-            while (j >= 0 && arr[j] > current) {
-                arr[j + 1] = arr[j];
-                j--;
-                swapped = true;
-            }
+      arr[j + 1] = current;
 
-            arr[j + 1] = current;
-
-            if (!swapped) {
-                break;
-            }
-        }
-
-        return arr;
+      if (!swapped) {
+        break;
+      }
     }
+
+    return arr;
+  }
 }
