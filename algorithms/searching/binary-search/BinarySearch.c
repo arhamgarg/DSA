60f3e83f798ad0ccc1aed2250acc6f30df7477cf
JestiferHarold <sanjay.pandian@tutamail.com>
2025-08-17 15:58:48 +0530

    Add: Binary Search in c and sorting algos in ts

--- algorithms/searching/binary-search/BinarySearch.c
+++ algorithms/searching/binary-search/BinarySearch.c
@@ -1,3 +1,17 @@
 int BinarySearch(int[] sortedArray, int target) {
-    
+    int low = 0;
+    int high = sizeof(sortedArray) / sizeof(sortedArray[0]);
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (sortedArray[mid] == target) {
+            return mid;
+        } else if (sortedArray[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return -1
 }
