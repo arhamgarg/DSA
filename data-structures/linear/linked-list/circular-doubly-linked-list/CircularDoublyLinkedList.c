eee0382c855f48ef666e389124f7986821ca0e94
Arham Garg <arhamvgarg@gmail.com>
2025-09-05 21:38:37 +0530

    Revert "Issues in Linked List (C, Java, Python)"

--- data-structures/linear/linked-list/circular-doubly-linked-list/CircularDoublyLinkedList.c
+++ data-structures/linear/linked-list/circular-doubly-linked-list/CircularDoublyLinkedList.c
@@ -19,7 +19,6 @@ struct DCL initList()
     struct DCL list = {NULL, NULL};
     return list;
 }
-//-------------Core-operations-------------
 
 struct node *createNode(int value)
 {
@@ -28,102 +27,6 @@ struct node *createNode(int value)
     new_Node->next = new_Node->prev = NULL;
     return new_Node;
 }
-
-void TraverseForward(struct DCL *list)
-{
-    if (list->head == NULL)
-    {
-        printf("Empty\n");
-        return;
-    }
-    struct node *temp = list->head;
-    do
-    {
-        printf("%d <-> ", temp->data);
-        temp = temp->next;
-    } while (temp != list->head);
-    printf("(back to head)\n");
-}
-
-void TraverseBackward(struct DCL *list)
-{
-    if (list->head == NULL)
-    {
-        printf("Empty\n");
-        return;
-    }
-    struct node *temp = list->tail;
-    do
-    {
-        printf("%d <-> ", temp->data);
-        temp = temp->prev;
-    } while (temp != list->tail);
-    printf("(back to tail)\n");
-}
-
-void isEmpty(struct DCL *list)
-{
-    printf("List %s empty\n", list->head ? "isn't" : "is");
-}
-
-void Search(struct DCL *list, int search_data)
-{
-    if (list->head == NULL)
-    {
-        printf("Empty\n");
-        return;
-    }
-    int pos = 0;
-    struct node *temp = list->head;
-    do
-    {
-        if (temp->data == search_data)
-        {
-            printf("Data %d found at position %d\n", temp->data, pos);
-            return;
-        }
-        pos++;
-        temp = temp->next;
-    } while (temp != list->head);
-    printf("Data %d not found\n", search_data);
-}
-
-int front(struct DCL *list)
-{
-    if (list->head == NULL)
-    {
-        printf("List empty\n");
-        return -1;
-    }
-    return list->head->data;
-}
-
-int back(struct DCL *list)
-{
-    if (list->tail == NULL)
-    {
-        printf("List empty\n");
-        return -1;
-    }
-    return list->tail->data;
-}
-
-int size(struct DCL *list)
-{
-    if (list->head == NULL)
-        return 0;
-
-    int count = 0;
-    struct node *temp = list->head;
-    do
-    {
-        count++;
-        temp = temp->next;
-    } while (temp != list->head);
-    return count;
-}
-
-//-------------Insertions-------------
 
 struct node *insert_First(struct DCL *list, int value)
 {
@@ -197,8 +100,6 @@ struct node *insert_At(struct DCL *list, int position, int value)
 
     return list->head;
 }
-
-//-------------Deletions-------------
 
 struct node *delete_First(struct DCL *list)
 {
@@ -276,132 +177,83 @@ struct node *delete_At(struct DCL *list, int position)
     return list->head;
 }
 
-struct node *deleteKey(struct DCL *list, int delete_data)
+void display(struct DCL *list)
 {
     if (list->head == NULL)
-        return NULL;
-
+    {
+        printf("Empty\n");
+        return;
+    }
     struct node *temp = list->head;
     do
     {
-        if (temp->data == delete_data)
-        { // handling edge cases here ....
-            if (temp == list->head && temp == list->tail)
-            {
-                printf("Deleted value: %d\n", temp->data); // single node condition
-                free(temp);
-                list->head = list->tail = NULL;
-                return NULL;
-            }
-            else if (temp == list->head)
-                return delete_First(list); // at the start
-            else if (temp == list->tail)
-                return delete_Last(list); // at last
-            else
-            {
-                temp->prev->next = temp->next;
-                temp->next->prev = temp->prev;
-                printf("Deleted value: %d\n", temp->data);
-                free(temp);
-                return list->head;
-            }
-        }
+        printf("%d <-> ", temp->data);
         temp = temp->next;
     } while (temp != list->head);
-
-    printf("Node with given data: %d not found\n", delete_data);
-    return list->head;
+    printf("(back to head)\n");
 }
-
-//-------------Utilties-------------
-
-struct node *Update(struct DCL *list, int update_val, int new_data)
+int front(struct DCL *list)
 {
     if (list->head == NULL)
     {
-        printf("Empty List\n");
-        return NULL;
+        printf("List empty\n");
+        return -1;
     }
+    return list->head->data;
+}
 
+int back(struct DCL *list)
+{
+    if (list->tail == NULL)
+    {
+        printf("List empty\n");
+        return -1;
+    }
+    return list->tail->data;
+}
+
+int size(struct DCL *list)
+{
+    if (list->head == NULL)
+        return 0;
+
+    int count = 0;
     struct node *temp = list->head;
     do
     {
-        if (temp->data == update_val)
-        {
-            temp->data = new_data;
-            printf("Node updated: %d -> %d\n", update_val, new_data);
-            return list->head;
-        }
+        count++;
         temp = temp->next;
     } while (temp != list->head);
-    printf("Node with given data: %d not found\n", update_val);
-    return list->head;
-}
-// mis-conceptions: swapping the head and tail doesn't mean reversed in DDL cuz of next and prev connections
-struct node *Reverse(struct DCL *list)
-{
-    if (list->head == NULL)
-    {
-        printf("Empty List\n");
-        return NULL;
-    }
-    struct node *current = list->head;
-    struct node *temp = NULL;
-    do
-    {
-        temp = current->prev;
-        current->prev = current->next;
-        current->next = temp;
-        current = current->prev;
-    } while (current != list->head);
-    temp = list->head;
-    list->head = list->tail;
-    list->tail = temp;
-
-    return list->head;
+    return count;
 }
 
 int main()
 {
     struct DCL list = initList();
-
     //---------------- Test ----------------
     insert_Last(&list, 10);
     insert_Last(&list, 20);
     insert_Last(&list, 30);
-    TraverseForward(&list);
+    display(&list);
 
     insert_First(&list, 5);
-    TraverseForward(&list);
+    display(&list);
 
     insert_At(&list, 2, 15);
-    TraverseForward(&list);
+    display(&list);
 
     printf("Front: %d\n", front(&list));
     printf("Back: %d\n", back(&list));
     printf("Size: %d\n", size(&list));
 
     delete_First(&list);
-    TraverseForward(&list);
+    display(&list);
 
     delete_Last(&list);
-    TraverseForward(&list);
+    display(&list);
 
     delete_At(&list, 1);
-    TraverseForward(&list);
-
-    // Try delete by key
-    deleteKey(&list, 15);
-    TraverseForward(&list);
-
-    // Try update
-    Update(&list, 20, 200);
-    TraverseForward(&list);
-
-    // Try reverse
-    Reverse(&list);
-    TraverseForward(&list);
-    TraverseBackward(&list);
+    display(&list);
 
     return 0;
 }
--- data-structures/linear/linked-list/circular-doubly-linked-list/CircularDoublyLinkedList.py
+++ data-structures/linear/linked-list/circular-doubly-linked-list/CircularDoublyLinkedList.py
@@ -1,287 +1,116 @@
-## python version 3.6 and later since formatted string print is used kindly check that if you are getting error regarding that
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+        self.val = val
         self.next = None
         self.prev = None
 
 
-class DCL:
+class CircularDoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
-    # ------------- Core Operations -------------
-    def TraverseForward(self):
+    def insertFirst(self, val):
+        new_node = Node(val)
         if self.head is None:
-            print("Empty")
-            return
-        temp = self.head
-        while True:
-            print(f"{temp.data} <-> ", end="")
-            temp = temp.next
-            if temp == self.head:
-                break
-        print("(back to head)")
+            new_node.next = new_node
+            new_node.prev = new_node
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            new_node.prev = self.tail
+            self.head.prev = new_node
+            self.tail.next = new_node
+            self.head = new_node
 
-    def TraverseBackward(self):
+    def insertLast(self, val):
+        new_node = Node(val)
         if self.head is None:
-            print("Empty")
-            return
-        temp = self.tail
-        while True:
-            print(f"{temp.data} <-> ", end="")
-            temp = temp.prev
-            if temp == self.tail:
-                break
-        print("(back to tail)")
+            new_node.next = new_node
+            new_node.prev = new_node
+            self.head = self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            new_node.next = self.head
+            self.tail.next = new_node
+            self.head.prev = new_node
+            self.tail = new_node
 
-    def isEmpty(self):
-        print("List isn't empty" if self.head else "List is empty")
-
-    def Search(self, search_data):  # 😎📔
+    def deleteFirst(self):
         if self.head is None:
-            print("Empty")
             return
-        pos = 0
-        temp = self.head
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = self.tail
+            self.tail.next = self.head
+
+    def deleteLast(self):
+        if self.head is None:
+            return
+        if self.head == self.tail:
+            self.head = self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = self.head
+            self.head.prev = self.tail
+
+    def insertAfter(self, u, v):
+        if self.head is None:
+            print(f"Value {u} not found in list.")
+            return
+        curr = self.head
         while True:
-            if temp.data == search_data:
-                print(f"Data {temp.data} found at position {pos}")
+            if curr.val == u:
+                new_node = Node(v)
+                new_node.next = curr.next
+                new_node.prev = curr
+                curr.next.prev = new_node
+                curr.next = new_node
+                if curr == self.tail:
+                    self.tail = new_node
                 return
-            pos += 1
-            temp = temp.next
-            if temp == self.head:
+            curr = curr.next
+            if curr == self.head:
                 break
-        print(f"Data {search_data} not found")
+        print(f"Value {u} not found in list.")
 
-    def front(self):
+    def linear_search(self, target):
         if self.head is None:
-            print("List empty")
             return -1
-        return self.head.data
+        index = 0
+        curr = self.head
+        while True:
+            if curr.val == target:
+                return index
+            curr = curr.next
+            index += 1
+            if curr == self.head:
+                break
+        return -1
 
-    def back(self):
-        if self.tail is None:
-            print("List empty")
-            return -1
-        return self.tail.data
-
-    def size(self):
+    def listSize(self):
         if self.head is None:
             return 0
-        count = 0
-        temp = self.head
+        size = 1
+        curr = self.head.next
+        while curr != self.head:
+            size += 1
+            curr = curr.next
+        return size
+
+    def isEmpty(self):
+        return self.head is None
+
+    def printList(self):
+        if self.head is None:
+            print("Empty list")
+            return
+        curr = self.head
         while True:
-            count += 1
-            temp = temp.next
-            if temp == self.head:
+            print(curr.val, end=" <-> ")
+            curr = curr.next
+            if curr == self.head:
                 break
-        return count
-
-    # ------------- Insertions -------------
-    def insert_First(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = self.tail = new_node
-            new_node.next = new_node.prev = new_node
-        else:
-            new_node.next = self.head
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.head.prev = new_node
-            self.head = new_node
-        return self.head
-
-    def insert_Last(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = self.tail = new_node
-            new_node.next = new_node.prev = new_node
-        else:
-            new_node.prev = self.tail
-            new_node.next = self.head
-            self.tail.next = new_node
-            self.head.prev = new_node
-            self.tail = new_node
-        return self.head
-
-    def insert_At(self, position, value):
-        if position == 0:
-            return self.insert_First(value)
-
-        temp = self.head
-        count = 0
-        while count < position - 1 and temp.next != self.head:
-            temp = temp.next
-            count += 1
-
-        if count != position - 1:
-            print("Position out of range")
-            return self.head
-
-        new_node = Node(value)
-        new_node.next = temp.next
-        new_node.prev = temp
-        temp.next.prev = new_node
-        temp.next = new_node
-
-        if temp == self.tail:
-            self.tail = new_node
-
-        return self.head
-
-    # ------------- Deletions -------------
-    def delete_First(self):
-        if self.head is None:
-            return None
-        temp = self.head
-        if self.head == self.tail:
-            print(f"Deleted value : {temp.data}")
-            self.head = self.tail = None
-            return None
-        print(f"Deleted value : {temp.data}")
-        self.head = self.head.next
-        self.tail.next = self.head
-        self.head.prev = self.tail
-        return self.head
-
-    def delete_Last(self):
-        if self.head is None:
-            return None
-        temp = self.tail
-        if self.head == self.tail:
-            print(f"Deleted value : {temp.data}")
-            self.head = self.tail = None
-            return None
-        print(f"Deleted value : {temp.data}")
-        self.tail = self.tail.prev
-        self.tail.next = self.head
-        self.head.prev = self.tail
-        return self.head
-
-    def delete_At(self, position):
-        if self.head is None:
-            return None
-        if position == 0:
-            return self.delete_First()
-
-        temp = self.head
-        count = 0
-        while count < position - 1 and temp.next != self.head:
-            temp = temp.next
-            count += 1
-
-        if count != position - 1 or temp.next == self.head:
-            print("Position out of range")
-            return self.head
-
-        to_delete = temp.next
-        print(f"Deleted: {to_delete.data}")
-
-        temp.next = to_delete.next
-        to_delete.next.prev = temp
-
-        if to_delete == self.tail:
-            self.tail = temp
-
-        return self.head
-
-    def deleteKey(self, delete_data):
-        if self.head is None:
-            return None
-        temp = self.head
-        while True:
-            if temp.data == delete_data:
-                if temp == self.head and temp == self.tail:
-                    print(f"Deleted value: {temp.data}")
-                    self.head = self.tail = None
-                    return None
-                elif temp == self.head:
-                    return self.delete_First()
-                elif temp == self.tail:
-                    return self.delete_Last()
-                else:
-                    temp.prev.next = temp.next
-                    temp.next.prev = temp.prev
-                    print(f"Deleted value: {temp.data}")
-                    return self.head
-            temp = temp.next
-            if temp == self.head:
-                break
-        print(f"Node with given data: {delete_data} not found")
-        return self.head
-
-    # ------------- Utilities -------------
-    def Update(self, update_val, new_data):
-        if self.head is None:
-            print("Empty List")
-            return None
-        temp = self.head
-        while True:
-            if temp.data == update_val:
-                temp.data = new_data
-                print(f"Node updated: {update_val} -> {new_data}")
-                return self.head
-            temp = temp.next
-            if temp == self.head:
-                break
-        print(f"Node with given data: {update_val} not found")
-        return self.head
-
-    def Reverse(self):
-        if self.head is None:
-            print("Empty List")
-            return None
-        current = self.head
-        temp = None
-        while True:
-            temp = current.prev
-            current.prev = current.next
-            current.next = temp
-            current = current.prev
-            if current == self.head:
-                break
-        temp = self.head
-        self.head = self.tail
-        self.tail = temp
-        return self.head
-
-
-# ---------------- Test ----------------
-if __name__ == "__main__":
-    list = DCL()
-
-    list.insert_Last(10)
-    list.insert_Last(20)
-    list.insert_Last(30)
-    list.TraverseForward()
-
-    list.insert_First(5)
-    list.TraverseForward()
-
-    list.insert_At(2, 15)
-    list.TraverseForward()
-
-    print("Front:", list.front())
-    print("Back:", list.back())
-    print("Size:", list.size())
-#😎📔
-    list.delete_First()
-    list.TraverseForward()
-
-    list.delete_Last()
-    list.TraverseForward()
-
-    list.delete_At(1)
-    list.TraverseForward()
-
-    list.deleteKey(15)
-    list.TraverseForward()
-
-    list.Update(20, 200)
-    list.TraverseForward()
-
-    list.Reverse()
-    list.TraverseForward()
-    list.TraverseBackward()
+        print("(back to head)")
