0edfcc5a60e10617fa08b40ac685e3b6a09c0650
JestiferHarold <sanjay.pandian@tutamail.com>
2025-09-10 09:38:10 -0400

    feat: binary tree and tree node in typescript

---
+++ data-structures/non-linear/node/TreeNode.ts
@@ -0,0 +1,37 @@
+class TreeNode {
+  private value: number;
+  private right: TreeNode | null;
+  private left: TreeNode | null;
+
+  constructor(
+    value: number,
+    right: TreeNode | null = null,
+    left: TreeNode | null = null,
+  ) {
+    this.value = value;
+    this.right = right;
+    this.left = left;
+  }
+
+  getValue(): number {
+    return this.value;
+  }
+
+  getRight(): TreeNode | null {
+    return this.right;
+  }
+
+  getLeft(): TreeNode | null {
+    return this.left;
+  }
+
+  setRight(node: TreeNode | null): void {
+    this.right = node;
+  }
+
+  setLeft(node: TreeNode | null): void {
+    this.left = node;
+  }
+}
+
+export { TreeNode };
