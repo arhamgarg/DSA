5c63abc39feeb247315f29e98cad32d67e3e56f6
Arham Garg <arhamvgarg@gmail.com>
2025-09-16 21:43:18 +0530

    style: Improve code style and readability

--- data-structures/non-linear/binary-tree/heap/min-heap/MinHeap.py
+++ data-structures/non-linear/binary-tree/heap/min-heap/MinHeap.py
@@ -4,12 +4,16 @@ class MinHeap:
         if arr:
             self.build_heap(arr)
 
-    def _left(self, i): return 2 * i + 1
-    def _right(self, i): return 2 * i + 2
-    def _parent(self, i): return (i - 1) // 2
+    def _left(self, i):
+        return 2 * i + 1
+
+    def _right(self, i):
+        return 2 * i + 2
+
+    def _parent(self, i):
+        return (i - 1) // 2
 
     def _heapify(self, i):
-        """Fix the heap property going downwards"""
         n = len(self.heap)
         smallest = i
         l, r = self._left(i), self._right(i)
@@ -24,8 +28,7 @@ class MinHeap:
             self._heapify(smallest)
 
     def build_heap(self, arr):
-        """Build heap in O(n) using bottom-up heapify"""
-        self.heap = arr[:]  
+        self.heap = arr[:]
         n = len(self.heap)
         for i in range(n // 2 - 1, -1, -1):
             self._heapify(i)
@@ -37,16 +40,18 @@ class MinHeap:
             return self.heap.pop()
 
         root = self.heap[0]
-        self.heap[0] = self.heap.pop()  # move last to root
+        self.heap[0] = self.heap.pop()
         self._heapify(0)
         return root
 
     def insert(self, key):
-        """Insert in O(log n)"""
         self.heap.append(key)
         i = len(self.heap) - 1
         while i > 0 and self.heap[self._parent(i)] > self.heap[i]:
-            self.heap[self._parent(i)], self.heap[i] = self.heap[i], self.heap[self._parent(i)]
+            self.heap[self._parent(i)], self.heap[i] = (
+                self.heap[i],
+                self.heap[self._parent(i)],
+            )
             i = self._parent(i)
 
     def get_min(self):
--- data-structures/non-linear/priority-queue/PriorityQueue.py
+++ data-structures/non-linear/priority-queue/PriorityQueue.py
@@ -4,10 +4,11 @@ class Node:
         self.priority = priority
         self.next = None
 
+
 class PriorityQueue:
     def __init__(self):
         self.head = None
-        self.size=0
+        self.size = 0
 
     def insert_item(self, data, priority):
         new_node = Node(data, priority)
@@ -21,24 +22,24 @@ class PriorityQueue:
                 current = current.next
             new_node.next = current.next
             current.next = new_node
-        self.size+=1
+        self.size += 1
 
     def remove_min(self):
         if not self.head:
             return None
         item = self.head.data
         self.head = self.head.next
-        self.size-=1
+        self.size -= 1
         return item
 
     def min_element(self):
         return None if not self.head else self.head.data
-    
+
     def min_key(self):
         return None if not self.head else self.head.priority
-    
+
     def _size(self):
         return self.size
-    
+
     def is_empty(self):
-        return self.size==0
+        return self.size == 0
