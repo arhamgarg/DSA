5d4561597c2f01fa737595dcbcca5e2b044c50d8
Arham Garg <arhamvgarg@gmail.com>
2025-10-29 20:56:29 +0530

    style: standardise indentation in TopologicalSort.java

--- algorithms/graph/topological-sort/TopologicalSort.java
+++ algorithms/graph/topological-sort/TopologicalSort.java
@@ -2,47 +2,47 @@ import java.util.*;
 import java.util.Stack;
 
 class Graph {
-    int V;
-    List<Integer>[] adj;
+  int V;
+  List<Integer>[] adj;
 
-    public Graph(int V) {
-        this.V = V;
-        adj = new ArrayList[V];
-        for (int i = 0; i < V; i++) {
-            adj[i] = new ArrayList<>();
-        }
+  public Graph(int V) {
+    this.V = V;
+    adj = new ArrayList[V];
+    for (int i = 0; i < V; i++) {
+      adj[i] = new ArrayList<>();
+    }
+  }
+
+  void addEdge(int u, int v) {
+    adj[u].add(v);
+  }
+
+  private void dfs(int u, boolean[] visited, Stack<Integer> stack) {
+    visited[u] = true;
+
+    for (int v : adj[u]) {
+      if (!visited[v]) {
+        dfs(v, visited, stack);
+      }
     }
 
-    void addEdge(int u, int v) {
-        adj[u].add(v);
+    stack.push(u);
+  }
+
+  void topologicalSort() {
+    boolean[] visited = new boolean[V];
+    Stack<Integer> stack = new Stack<>();
+
+    for (int i = 0; i < V; i++) {
+      if (!visited[i]) {
+        dfs(i, visited, stack);
+      }
     }
 
-    private void dfs(int u, boolean[] visited, Stack<Integer> stack) {
-        visited[u] = true;
-
-        for (int v : adj[u]) {
-            if (!visited[v]) {
-                dfs(v, visited, stack);
-            }
-        }
-
-        stack.push(u);
+    while (!stack.isEmpty()) {
+      System.out.print(stack.pop() + " ");
     }
 
-    void topologicalSort() {
-        boolean[] visited = new boolean[V];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                dfs(i, visited, stack);
-            }
-        }
-
-        while (!stack.isEmpty()) {
-            System.out.print(stack.pop() + " ");
-        }
-
-        System.out.println();
-    }
+    System.out.println();
+  }
 }
