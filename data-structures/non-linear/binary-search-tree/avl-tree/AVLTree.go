27a19fd6e804284d8f59c3c118c0890940d8e519
Arham Garg <arhamvgarg@gmail.com>
2025-11-26 19:26:41 +0530

    style: reformat Java and Go AVLTree source files

--- data-structures/non-linear/binary-search-tree/avl-tree/AVLTree.go
+++ data-structures/non-linear/binary-search-tree/avl-tree/AVLTree.go
@@ -2,7 +2,6 @@ package main
 
 import "fmt"
 
-
 type Node struct {
 	data   int
 	left   *Node
--- data-structures/non-linear/binary-search-tree/avl-tree/AVLTree.java
+++ data-structures/non-linear/binary-search-tree/avl-tree/AVLTree.java
@@ -1,116 +1,114 @@
 class Node {
-    int data;
-    Node left;
-    Node right;
-    int height;
+  int data;
+  Node left;
+  Node right;
+  int height;
 
-    public Node(int data) {
-        this.data = data;
-        this.left = this.right = null;
-        height = 1;
-    }
+  public Node(int data) {
+    this.data = data;
+    this.left = this.right = null;
+    height = 1;
+  }
 }
 
 public class AVLTree {
-    Node root;
+  Node root;
 
-    public AVLTree() {
-        root = null;
+  public AVLTree() {
+    root = null;
+  }
+
+  int height(Node node) {
+    return (node == null) ? 0 : node.height;
+  }
+
+  int balanceFactor(Node node) {
+    return (node == null) ? 0 : height(node.left) - height(node.right);
+  }
+
+  Node rightRotate(Node y) {
+    Node x = y.left;
+    Node T2 = x.right;
+
+    x.right = y;
+    y.left = T2;
+
+    y.height = Math.max(height(y.left), height(y.right)) + 1;
+    x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+    return x;
+  }
+
+  Node leftRotate(Node x) {
+    Node y = x.right;
+    Node T2 = y.left;
+
+    y.left = x;
+    x.right = T2;
+
+    x.height = Math.max(height(x.left), height(x.right)) + 1;
+    y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+    return y;
+  }
+
+  Node insert(Node node, int value) {
+    if (node == null) {
+      return new Node(value);
     }
 
-
-    int height(Node node) {
-        return (node == null) ? 0 : node.height;
+    if (value < node.data) {
+      node.left = insert(node.left, value);
+    } else if (value > node.data) {
+      node.right = insert(node.right, value);
+    } else {
+      return node;
     }
 
-    int balanceFactor(Node node) {
-        return (node == null) ? 0 : height(node.left) - height(node.right);
+    node.height = 1 + Math.max(height(node.left), height(node.right));
+
+    int balance = balanceFactor(node);
+
+    // left left
+    if (balance > 1 && value < node.left.data) {
+      return rightRotate(node);
     }
 
-    Node rightRotate(Node y) {
-        Node x = y.left;
-        Node T2 = x.right;
-
-        x.right = y;
-        y.left = T2;
-
-        y.height = Math.max(height(y.left), height(y.right)) + 1;
-        x.height = Math.max(height(x.left), height(x.right)) + 1;
-
-        return x;
+    // light right
+    if (balance < -1 && value > node.right.data) {
+      return leftRotate(node);
     }
 
-    Node leftRotate(Node x) {
-        Node y = x.right;
-        Node T2 = y.left;
-
-        y.left = x;
-        x.right = T2;
-
-        x.height = Math.max(height(x.left), height(x.right)) + 1;
-        y.height = Math.max(height(y.left), height(y.right)) + 1;
-
-        return y;
+    // left right
+    if (balance > 1 && value > node.left.data) {
+      node.left = leftRotate(node.left);
+      return rightRotate(node);
     }
 
-    Node insert(Node node, int value) {
-        if (node == null) {
-            return new Node(value);
-        }
-
-        if (value < node.data) {
-            node.left = insert(node.left, value);
-        } else if (value > node.data) {
-            node.right = insert(node.right, value);
-        } else {
-            return node;
-        }
-
-        node.height = 1 + Math.max(height(node.left), height(node.right));
-
-        int balance = balanceFactor(node);
-
-        // left left
-        if (balance > 1 && value < node.left.data) {
-            return rightRotate(node);
-        }
-
-        // light right
-        if (balance < -1 && value > node.right.data) {
-            return leftRotate(node);
-        }
-
-        // left right
-        if (balance > 1 && value > node.left.data) {
-            node.left = leftRotate(node.left);
-            return rightRotate(node);
-        }
-
-        // right left
-        if (balance < -1 && value < node.right.data) {
-            node.right = rightRotate(node.right);
-            return leftRotate(node);
-        }
-
-        return node;
+    // right left
+    if (balance < -1 && value < node.right.data) {
+      node.right = rightRotate(node.right);
+      return leftRotate(node);
     }
 
-    void insert(int value) {
-        root = insert(root, value);
-    }
+    return node;
+  }
 
-    void inorder() {
-        inorderTraversal(root);
-        System.out.println();
-    }
+  void insert(int value) {
+    root = insert(root, value);
+  }
 
-    void inorderTraversal(Node root) {
-        if (root == null)
-            return;
+  void inorder() {
+    inorderTraversal(root);
+    System.out.println();
+  }
 
-        inorderTraversal(root.left);
-        System.out.print(root.data + " ");
-        inorderTraversal(root.right);
-    }
+  void inorderTraversal(Node root) {
+    if (root == null)
+      return;
 
+    inorderTraversal(root.left);
+    System.out.print(root.data + " ");
+    inorderTraversal(root.right);
+  }
 }
