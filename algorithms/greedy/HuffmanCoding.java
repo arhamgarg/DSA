475d3c490a862c8f3cf3f009bf3f013be622ccc3
Arham Garg <arhamvgarg@gmail.com>
2026-03-01 23:49:58 +0530

    style: reformat Java source files with consistent indentation

--- algorithms/greedy/HuffmanCoding.java
+++ algorithms/greedy/HuffmanCoding.java
@@ -1,99 +1,98 @@
 import java.util.*;
 
 class Node {
-    char ch;
-    int freq;
-    Node left;
-    Node right;
+  char ch;
+  int freq;
+  Node left;
+  Node right;
 
-    Node(char ch, int freq) {
-        this.ch = ch;
-        this.freq = freq;
-        this.left = null;
-        this.right = null;
-    }
+  Node(char ch, int freq) {
+    this.ch = ch;
+    this.freq = freq;
+    this.left = null;
+    this.right = null;
+  }
 
-    Node(char ch, int freq, Node left, Node right) {
-        this.ch = ch;
-        this.freq = freq;
-        this.left = left;
-        this.right = right;
-    }
+  Node(char ch, int freq, Node left, Node right) {
+    this.ch = ch;
+    this.freq = freq;
+    this.left = left;
+    this.right = right;
+  }
 }
 
 public class HuffmanCoding {
-    public static void printCodes(Node root, String str, Map<Character, String> huffmanCode) {
-        if (root == null)
-            return;
+  public static void printCodes(
+      Node root, String str, Map<Character, String> huffmanCode) {
+    if (root == null)
+      return;
 
-        if (root.left == null && root.right == null) {
-            huffmanCode.put(root.ch, str);
-        }
-
-        printCodes(root.left, str + "0", huffmanCode);
-        printCodes(root.right, str + "1", huffmanCode);
+    if (root.left == null && root.right == null) {
+      huffmanCode.put(root.ch, str);
     }
 
-    public static void buildHuffmanTree(String text) {
-        Map<Character, Integer> freq = new HashMap<>();
-        for (char ch : text.toCharArray()) {
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
-        }
+    printCodes(root.left, str + "0", huffmanCode);
+    printCodes(root.right, str + "1", huffmanCode);
+  }
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(
-                (l, r) -> l.freq - r.freq);
-
-        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
-            pq.add(new Node(entry.getKey(), entry.getValue()));
-        }
-
-        while (pq.size() != 1) {
-            Node left = pq.poll();
-            Node right = pq.poll();
-
-            int sum = left.freq + right.freq;
-
-            pq.add(new Node('\0', sum, left, right));
-        }
-
-        Node root = pq.peek();
-
-        Map<Character, String> huffmanCode = new HashMap<>();
-        printCodes(root, "", huffmanCode);
-
-        System.out.println("Huffman Codes:");
-        for (Map.Entry<Character, String> entry : huffmanCode.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
-
-        System.out.println("\nOriginal string:");
-        System.out.println(text);
-
-        StringBuilder encodedStr = new StringBuilder();
-        for (char ch : text.toCharArray()) {
-            encodedStr.append(huffmanCode.get(ch));
-        }
-
-        System.out.println("\nEncoded string:");
-        System.out.println(encodedStr);
-
-        System.out.println("\nDecoded string:");
-        Node curr = root;
-
-        for (char bit : encodedStr.toString().toCharArray()) {
-
-            if (bit == '0') {
-                curr = curr.left;
-            } else {
-                curr = curr.right;
-            }
-
-            if (curr.left == null && curr.right == null) {
-                System.out.print(curr.ch);
-                curr = root;
-            }
-        }
-
-        System.out.println();
+  public static void buildHuffmanTree(String text) {
+    Map<Character, Integer> freq = new HashMap<>();
+    for (char ch : text.toCharArray()) {
+      freq.put(ch, freq.getOrDefault(ch, 0) + 1);
     }
+
+    PriorityQueue<Node> pq = new PriorityQueue<>((l, r) -> l.freq - r.freq);
+
+    for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+      pq.add(new Node(entry.getKey(), entry.getValue()));
+    }
+
+    while (pq.size() != 1) {
+      Node left = pq.poll();
+      Node right = pq.poll();
+
+      int sum = left.freq + right.freq;
+
+      pq.add(new Node('\0', sum, left, right));
+    }
+
+    Node root = pq.peek();
+
+    Map<Character, String> huffmanCode = new HashMap<>();
+    printCodes(root, "", huffmanCode);
+
+    System.out.println("Huffman Codes:");
+    for (Map.Entry<Character, String> entry : huffmanCode.entrySet()) {
+      System.out.println(entry.getKey() + " " + entry.getValue());
+    }
+
+    System.out.println("\nOriginal string:");
+    System.out.println(text);
+
+    StringBuilder encodedStr = new StringBuilder();
+    for (char ch : text.toCharArray()) {
+      encodedStr.append(huffmanCode.get(ch));
+    }
+
+    System.out.println("\nEncoded string:");
+    System.out.println(encodedStr);
+
+    System.out.println("\nDecoded string:");
+    Node curr = root;
+
+    for (char bit : encodedStr.toString().toCharArray()) {
+      if (bit == '0') {
+        curr = curr.left;
+      } else {
+        curr = curr.right;
+      }
+
+      if (curr.left == null && curr.right == null) {
+        System.out.print(curr.ch);
+        curr = root;
+      }
+    }
+
+    System.out.println();
+  }
 }
--- algorithms/sorting/bucket-sort/BucketSort.java
+++ algorithms/sorting/bucket-sort/BucketSort.java
@@ -1,43 +1,42 @@
 import java.util.ArrayList;
 
 public class BucketSort {
-    public static void insertionSort(ArrayList<Double> bucket) {
-        for (int i = 1; i < bucket.size(); i++) {
-            double key = bucket.get(i);
-            int j = i - 1;
+  public static void insertionSort(ArrayList<Double> bucket) {
+    for (int i = 1; i < bucket.size(); i++) {
+      double key = bucket.get(i);
+      int j = i - 1;
 
-            while (j >= 0 && bucket.get(j) > key) {
-                bucket.set(j + 1, bucket.get(j));
-                j--;
-            }
+      while (j >= 0 && bucket.get(j) > key) {
+        bucket.set(j + 1, bucket.get(j));
+        j--;
+      }
 
-            bucket.set(j + 1, key);
-        }
+      bucket.set(j + 1, key);
+    }
+  }
+
+  public static void bucketSort(int[] arr) {
+    int n = arr.length;
+    ArrayList<Double>[] buckets = new ArrayList[n];
+
+    for (int i = 0; i < n; i++) {
+      buckets[i] = new ArrayList<>();
     }
 
-    public static void bucketSort(int[] arr) {
-        int n = arr.length;
-        ArrayList<Double>[] buckets = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) {
-            buckets[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < n; i++) {
-            int bi = n * arr[i];
-            buckets[bi].add((double) arr[i]);
-        }
-
-        for (int i = 0; i < n; i++) {
-            insertionSort(buckets[i]);
-        }
-
-        int index = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < buckets[i].size(); j++) {
-                arr[index++] = buckets[i].get(j).intValue();
-            }
-        }
+    for (int i = 0; i < n; i++) {
+      int bi = n * arr[i];
+      buckets[bi].add((double) arr[i]);
     }
 
+    for (int i = 0; i < n; i++) {
+      insertionSort(buckets[i]);
+    }
+
+    int index = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < buckets[i].size(); j++) {
+        arr[index++] = buckets[i].get(j).intValue();
+      }
+    }
+  }
 }
