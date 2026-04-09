998692ea2e221354219fcf92bc6568efe7b4eb70
Arham Garg <arhamvgarg@gmail.com>
2026-04-09 12:40:32 +0530

    fix: pass weights and benefits by reference in binaryKnapsack for efficiency

--- algorithms/dynamic-programming/01Knapsack.cpp
+++ algorithms/dynamic-programming/01Knapsack.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void binaryKnapsack(int W, vector<int> weights, vector<int> benefits,
-                    vector<int> B) {
+void binaryKnapsack(int W, vector<int> &weights, vector<int> &benefits,
+                    vector<int> &B) {
   for (int k = 0; k < (int)weights.size(); k++) {
     int w_k = weights[k];
     int b_k = benefits[k];
