624a2c9f4f7b8531dbf5309ad7bf90154b707244
Arham Garg <arhamvgarg@gmail.com>
2026-09-06 19:44:57 +0530

    feat: add C++ BFS and normalize graph algorithm filenames

---
+++ algorithms/graph/bfs/BFS.cpp
@@ -0,0 +1,28 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+vector<int> bfs(const vector<vector<int>> &graph, int start) {
+  vector<int> traversal;
+  vector<bool> visited(graph.size(), false);
+  queue<int> nodes;
+
+  nodes.push(start);
+  visited[start] = true;
+
+  while (!nodes.empty()) {
+    int node = nodes.front();
+
+    nodes.pop();
+    traversal.push_back(node);
+
+    for (int neighbor : graph[node]) {
+      if (!visited[neighbor]) {
+        visited[neighbor] = true;
+        nodes.push(neighbor);
+      }
+    }
+  }
+
+  return traversal;
+}
