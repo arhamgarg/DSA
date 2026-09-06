70287e6cdea0b62c2d8146268f8788615487484c
Arham Garg <arhamvgarg@gmail.com>
2026-09-06 20:10:30 +0530

    feat: add Disjoint Set Union in C++

---
+++ data-structures/non-linear/disjoint-set/DisjointSet.cpp
@@ -0,0 +1,42 @@
+#include <numeric>
+#include <utility>
+#include <vector>
+using namespace std;
+
+class DisjointSet {
+  vector<int> parent;
+  vector<int> rank;
+
+public:
+  explicit DisjointSet(int size) : parent(size), rank(size, 0) {
+    iota(parent.begin(), parent.end(), 0);
+  }
+
+  int find(int value) {
+    if (parent[value] != value) {
+      parent[value] = find(parent[value]);
+    }
+
+    return parent[value];
+  }
+
+  void unite(int first, int second) {
+    first = find(first);
+    second = find(second);
+
+    if (first == second) {
+      return;
+    }
+
+    if (rank[first] < rank[second]) {
+      swap(first, second);
+    }
+
+    parent[second] = first;
+    if (rank[first] == rank[second]) {
+      rank[first]++;
+    }
+  }
+
+  bool connected(int first, int second) { return find(first) == find(second); }
+};
